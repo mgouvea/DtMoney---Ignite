554a510773931e024bc92f65f43e182ffeb8ab73
Mateus <mateuscg42@gmail.com>
2022-01-23 15:32:46 -0300

    Transaction Modal

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
+import Modal from 'react-modal'
 import logoImg from '../../assets/logo.svg';
+
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onRequestOpenTransactionModal: () => void;
+}
+
+export function Header({onRequestOpenTransactionModal}:HeaderProps) {
+
+  
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onRequestOpenTransactionModal} >Nova transação</button>
+
+      
+
       </Content>
     </Container>
   );
